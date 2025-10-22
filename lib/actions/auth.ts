da88898a34024b8cd10b7c76ca77f5a95d399a61
Tomas Cabagay, Jr <tomas.cabagay@gmail.com>
2025-10-22 11:05:08 +0800

    Implement QStash workflows for user onboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "@/lib/workflow";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
@@ -71,6 +73,14 @@ export const signUp = async (params: AuthCredentials) => {
     };
 
     await db.insert(users).values(newUser);
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
+    });
 
     await signInWithCredentials({ email, password });
     return { success: true };
